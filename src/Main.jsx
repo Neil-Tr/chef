6a374b3d9c9afc28e7e000930260a88db70826b2
neilt <neiltran.syd@gmail.com>
2025-08-29 21:54:54 +1000

    update-5

--- src/Main.jsx
+++ src/Main.jsx
@@ -32,7 +32,7 @@ export default function Main() {
       <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
-          placeholder="e.g. oregano"
+          placeholder="e.g. butter"
           aria-label="Add ingredient"
           name="ingredient"
         />
